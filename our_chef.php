f46003e93caa546a1324642388b7e9cfd118e3aa
tonmoyaust37 <tonmoymohajan221@gmail.com>
2019-04-17 00:47:36 +0600

    chef_details

---
+++ our_chef.php
@@ -0,0 +1,104 @@
+
+<?php 
+	include 'header.php'; 
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title></title>
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/our_chef.css">
+	</head>
+	
+	<body>
+		<div class="teachers">
+			<div class="container style="padding-left: 0px;padding-right: 0px;"">
+				<div class="teach-head">
+					<h3>OUR Chef</h3>
+					<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis 
+					praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate.</p>
+				</div>
+				<div class="team-grids">
+					<div class="col-md-3 team-grid text-center">
+						<div class="team-img">
+							<img src="chef_img/1.png" alt=""/>
+							<h3>FEDERICA</h3>
+							<h4>Executive Chef</h4>
+							<ul>
+								<li><a class="fb" href="#"></a></li>
+								<li><a class="twitt" href="#"></a></li>
+								<li><a class="goog" href="#"></a></li>
+								<li><a class="drib" href="#"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid text-center">
+						<div class="team-img">
+							<img src="chef_img/2.png" alt=""/>
+							<h3>PATRICK</h3>
+							<h4>Sous Chef</h4>
+							
+							<ul>
+								<li><a class="fb" href="#"></a></li>
+								<li><a class="twitt" href="#"></a></li>
+								<li><a class="goog" href="#"></a></li>
+								<li><a class="drib" href="#"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid text-center">
+						<div class="team-img">
+							<img src="chef_img/3.png" alt=""/>
+							<h3>JANE ASHER</h3>
+							<h4>Expediter</h4>
+							
+							<ul>
+								<li><a class="fb" href="#"></a></li>
+								<li><a class="twitt" href="#"></a></li>
+								<li><a class="goog" href="#"></a></li>
+								<li><a class="drib" href="#"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-3 team-grid text-center">
+						<div class="team-img">
+							<img src="chef_img/4.png" alt=""/>
+							<h3>VICTORIA</h3>
+							<h4>Vegetable Cook</h4>
+							
+							<ul>
+								<li><a class="fb" href="#"></a></li>
+								<li><a class="twitt" href="#"></a></li>
+								<li><a class="goog" href="#"></a></li>
+								<li><a class="drib" href="#"></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		
+		
+		
+		
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+	</body>
+	
+</html>
+
+
+
+
+
+
