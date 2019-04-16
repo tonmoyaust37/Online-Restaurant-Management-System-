45ec080fc3bff653325ec028ec719a35d1790225
tonmoyaust37 <tonmoymohajan221@gmail.com>
2019-04-17 02:54:07 +0600

    new review page added

---
+++ see_review.php
@@ -0,0 +1,28 @@
+<?php
+	include 'header2.php';
+?>
+  
+  <div id="fb-root">
+	  </div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2"></script>
+		
+		<h2 class="review_title">
+			Food review on Facebook
+		</h2>
+		</br>
+		
+		</br>
+		
+		<div class="review1">
+			<div class="fb-post" data-href="https://www.facebook.com/photo.php?fbid=1834026143572710&amp;set=gm.1659166324195847&amp;type=3&amp;eid=ARCPlN_mPROO5GRri4HrpeIRvWifcnI2HxFOvKTORYXePnTRvCFXC6rfUU_LzEVkPlHUwzJQ1mETBVuC&amp;ifg=1" data-width="750" data-show-text="true"><blockquote cite="https://www.facebook.com/photo.php?fbid=1834026143572710&amp;set=gm.1659166324195847&amp;type=3" class="fb-xfbml-parse-ignore"><p>Sultan&#039;s Dine is one of the best kacchi places in dhaka without a doubt. Their food is incredibly delicious and...</p>Posted by <a href="#" role="button">Noshin Tarannum</a> on&nbsp;<a href="https://www.facebook.com/photo.php?fbid=1834026143572710&amp;set=gm.1659166324195847&amp;type=3">Saturday, May 19, 2018</a></blockquote></div>
+		</div>
+		
+		
+		<div class="review2">
+			<div class="fb-post" data-href="https://www.facebook.com/photo.php?fbid=499793787095641&amp;set=gm.1878023732310104&amp;type=3&amp;theater&amp;ifg=1" data-width="750" data-show-text="true"><blockquote cite="https://www.facebook.com/photo.php?fbid=499793787095641&amp;set=gm.1878023732310104&amp;type=3" class="fb-xfbml-parse-ignore">				
+				<p>Pizza ❤ another name of love 😋when i saw  the positive  review than without  wasting  my tym i went there and took e...</p>Posted by <a href="https://www.facebook.com/nirajaman.jaman.7">Rounok Rehenumah</a> on&nbsp;<a href="https://www.facebook.com/photo.php?fbid=499793787095641&amp;set=gm.1878023732310104&amp;type=3">Monday, October 29, 2018</a></blockquote></div>
+		</div>
+		
+<?php
+	include 'footer.php';
+?>
