39996972db03912e7f46fd91489ea078cb67d4ac
tonmoyaust37 <tonmoymohajan221@gmail.com>
2019-04-16 22:01:43 +0600

    header portion includes(nav)

---
+++ header.php
@@ -0,0 +1,97 @@
+<?php
+	
+	
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">	
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title></title>
+		
+        <!-- Bootstrap -->
+        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"> -->
+		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"> -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+		
+		<!-- All Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Coiny" rel="stylesheet">
+		<!-- font-family: 'Coiny', cursive; -->
+		
+		
+		
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" type="text/css" href="css/style2.css">
+	</head>
+	
+    <body>
+		
+		<div class="header_Section">
+			<!-- navbar-light bg-light  -->
+			<nav class="navbar navbar-expand-lg fixed-top" id="nav">
+				<a class="navbar-brand" href="#"><img src="img/logo3.png" class="img-fluid" alt="">
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active">
+							<a class="nav-link" href="test1.php">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="food_menu.php">Menu</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="login_system.php">
+								<?php
+									if(!empty($_SESSION['email'])){
+										$_SESSION['fromMainPage']='1';
+										echo "Logout";
+									}
+									else{
+										echo "Login";
+										}
+								?>
+								
+								
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Gallery</a>
+						</li>
+						<li class="dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								About Us
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown" id="dropdownId">
+								<a class="dropdown-item" href="test1.php#our_chef">Our Chefs</a>
+								<a class="dropdown-item" href="#">Free Recipe</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">Something else here</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="contact_us.php">Contact Us</a>
+						</li>
+					</ul>
+					<!-- <form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form> -->
+				</div>
+			</nav>
+			
+		</div>
